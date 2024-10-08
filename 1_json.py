35d5097b74d9743d16535a8d81b37526da1e5c88
Jenbati <jen.bati16@gmail.com>
2024-10-08 11:36:31 +0545

    initial commit

---
+++ 1_json.py
@@ -0,0 +1,4 @@
+#JSON
+# JavaScript Object Notation
+# JSON is a file extension for sharing data (information) between frontend and backend and between backend and backend. 
+# JSON is very much similar to python dictionary having keys and values where keys must be string enclosed in doble quotation mark. and values can be following datatype.
